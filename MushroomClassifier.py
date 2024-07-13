5d07572ff35278ebc3dd0d8cebee6798e3e281dc
nico <nikiapple1008@icloud.com>
2024-07-13 19:15:30 +0300

    ml model created

---
+++ MushroomClassifier.py
@@ -0,0 +1,14 @@
+from torch import nn
+
+class MushroomClassifier(nn.Module):
+    def __init__(self, input_dim, hidden_dim, output_dim):
+        super(MushroomClassifier, self).__init__()
+        self.fc1 = nn.Linear(input_dim, hidden_dim)
+        self.relu = nn.ReLU()
+        self.fc2 = nn.Linear(hidden_dim, output_dim)
+    
+    def forward(self, x):
+        out = self.fc1(x)
+        out = self.relu(out)
+        out = self.fc2(out)
+        return out
